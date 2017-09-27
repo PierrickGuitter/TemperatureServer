03e03233529098305bca0224754f921f4c8d158d
PierrickGuitter <root@beaglebone.localdomain>
2017-09-27 20:39:43 +0200

    Exceptions handling (IOError) for file opening and documentation fixing

--- src/LEDTemp.py
+++ src/LEDTemp.py
@@ -1,3 +1,25 @@
+'''My very own temperature Server
+Copyright 2017, Pierrick Guitter
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.'''
+
+
 import Adafruit_BBIO.ADC as ADC
 import Adafruit_BBIO.GPIO as GPIO
 import numpy, datetime, time
@@ -5,35 +27,35 @@ import numpy, datetime, time
 
 tempsensor='P9_39'
 GridLed=numpy.array(['P8_8','P8_7','P8_10','P8_9','P8_12','P8_14','P8_15','P8_17','P8_18'])
-#  _______________________________________________________________________
-# | BLUE | BLUE | GREEN | GREEN | GREEN | YELLOW | YELLOW |  RED  |  RED  |
-# | P8_8 | P8_7 | P8_10 | P8_9  | P8_12 | P8_14  | P8_15  | P8_17 | P8_18 |
-#  -----------------------------------------------------------------------
-#
+
+'''
+  _______________________________________________________________________
+ | BLUE | BLUE | GREEN | GREEN | GREEN | YELLOW | YELLOW |  RED  |  RED  |
+ | P8_8 | P8_7 | P8_10 | P8_9  | P8_12 | P8_14  | P8_15  | P8_17 | P8_18 |
+  -----------------------------------------------------------------------
+'''
 
 def GPIOSetup():
-	"""Setting Up all LEDs as GPIO output"""
+	'''Setting Up all LEDs as GPIO output'''
 	for x in range(0, 9):
 		GPIO.setup(GridLed[x], GPIO.OUT)
 
 def GPIOTurnOff():
-	"""All LEDs turned off"""
+	'''All LEDs turned off'''
 	for x in range(0,9):
 		GPIO.output(GridLed[x], GPIO.LOW)
 	time.sleep(2)
 
-
 def TempCalc():
-	"""Define and print temperature from sensor"""
+	'''Define and print temperature from sensor'''
 	#GPIOTurnOff()
 	ADC.setup()
 	TEMP=(ADC.read(tempsensor) * 1800 - 500) / 10
-#print("%f" % ADC.read(tempsensor))
-	print("===== tmp = %.2f =====" % TEMP)
+	print("tmp = %.2f" % TEMP)
 	return TEMP
 
 def Thermometer():
-	"""Considering the value of TMP sensor, turn on LEDs or not"""
+	'''Considering the value of TMP sensor, turn on LEDs or not'''
 	TEMP=TempCalc()
 	if TEMP <= 10:
 		GPIO.output(GridLed[0], GPIO.HIGH)
@@ -79,33 +101,40 @@ def Thermometer():
 			GPIO.output(GridLed[x], GPIO.HIGH)
 
 def TempLogging():
-	"""Log all temperature every half hour, in TempLog.log"""
+	'''Log all temperature every half hour, in TempLog.log.
+	Give also food to the datafile that help generates a temperature curb every day'''
+
 	#GPIOTurnOff()
 	TEMP=TempCalc()
 	date=time.strftime("%a %b  %-d %H:%M:%S CEST %Y")
 	now=datetime.datetime.now()
 	LimitHour=now.replace(hour=23, minute=27)
-	FILE=open("TempLog.log",'a')
-	FILE.write(date)
-	FILE.write('\t')
-	FILE.write("TEMPERATURE = ")
-	FILE.write(str(TEMP))
-	FILE.write('\n')
+	
+	try:
+		with open("TempLog.log",'a') as ExternLog:
+			ExternLog.write(date)
+			ExternLog.write('\t')
+			ExternLog.write("TEMPERATURE = ")
+			ExternLog.write(str(TEMP))
+			ExternLog.write('\n')
+			if LimitHour<now:
+				ExternLog.write('\n')
+	except IOError:
+		print("Opening Templog failed")
+		
 
-	if LimitHour<now:
-		FILE.write('\n')
-	FILE.close()
-
-	F=open("DataFile.dat",'a')
-	F.write(time.strftime("%H.%M"))
-	F.write('\t')
-	F.write(str(TEMP))
-	F.write('\n')
-	F.close()
+	try:
+		with open("DataFile.dat",'a') as DailyData:
+			DailyData.write(time.strftime("%H.%M"))
+			DailyData.write('\t')
+			DailyData.write(str(TEMP))
+			DailyData.write('\n')
+	except IOError:
+		print("Opening Datafile failed")
 
 
 def DateCalc():
-	"""define if now() is between xx:28 and xx:31. If so, log it"""
+	'''define if now() is between xx:28 and xx:31. If so, log it'''
 	now=datetime.datetime.now()
 	LogUPtimehalf=now.replace(minute=31)
 	LogDOWNtimehalf=now.replace(minute=28)
@@ -114,22 +143,24 @@ def DateCalc():
 	if (LogDOWNtimehalf<now and now<LogUPtimehalf) or (LogDOWNtimeNewHour<now and now<LogUPtimeNewHour):
 		TempLogging()
 
-def webServerRequest():
-	"""WebServer takes its info from this request file"""
-	TEMP=TempCalc()
-	F=open("/var/www/request.txt",'w')
-	F.write("date :\t")
-        F.write(time.strftime("%d:%m:%y--%H:%M"))
-        F.write('\t')
-	F.write("temperature :\t")
-        F.write(str(TEMP))
-        F.write('\n')
-        F.close()
 
+def webServerRequest():
+	'''WebServer takes its info from this request file'''
+	TEMP=TempCalc()
+	try:
+		with open("/var/www/request.txt",'w') as webTemp:
+			webTemp.write("date :\t")
+			webTemp.write(time.strftime("%d:%m:%y--%H:%M"))
+			webTemp.write('\t')
+			webTemp.write("temperature :\t")
+			webTemp.write(str(TEMP))
+			webTemp.write('\n')
+	except IOError:
+		print("Opening request.txt failed")
 
 
 if __name__ == '__main__':
-	"""Infinite loop calculating temp and assigning LEDs every two minutes"""
+	'''Infinite loop calculating temp, assigning LEDs and logging every two minutes'''
 	while True:
 		GPIOSetup()
 		Thermometer()
